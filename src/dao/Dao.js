bb6e59123f27d5ea7b94d989b60aae855688340d
Jérémy Touzy <jtouzy2@gmail.com>
2018-12-26 10:59:32 +0100

    feat: add dao findOne() + QueryBuilder impl

--- src/dao/Dao.js
+++ src/dao/Dao.js
@@ -1,7 +1,31 @@
+const DataProxy = require('../data/DataProxy')
+const QueryBuilder = require('../query/QueryBuilder')
+
 class Dao {
-  constructor(context, databaseProxy) {
+  constructor(context) {
+    if (typeof context === 'undefined') {
+      throw new Error('Missing context object in dao instance. Maybe you forgot to call super constructor with context ?')
+    }
     this.$context = context
-    this.$databaseProxy = databaseProxy
+    this.$dataProxy = new DataProxy()
+    this.$queryBuilder = new QueryBuilder()
+  }
+  async findOne(identifier) {
+    const conditionsObject = this.$getConditionsObjectFromArgument(identifier)
+    const query = this.$queryBuilder.getSelectQueryWithConditionsObject(this.$tableMetadata, conditionsObject)
+    const result = await this.$databaseProxy.query(query)
+    return this.$dataProxy.databaseToObject(result)
+  }
+  $getConditionsObjectFromArgument(argument) {
+    if (typeof argument === 'string') {
+      return { id: argument } // TODO make with metadata columsn
+    } else if (typeof argument === 'object') {
+      return this.$dataProxy.objectToDatabase(argument)
+    } else if (typeof argument === 'function') {
+      return this.$dataProxy.objectToDatabase(argument())
+    } else {
+      return argument
+    }
   }
 }
 
---
+++ src/query/QueryBuilder.js
@@ -0,0 +1,30 @@
+class QueryBuilder {
+  constructor() {
+  }
+  getSelectQueryWithConditionsObject(tableMetadata, conditionsObject) {
+    const conditions = this.getConditionsWithObject(conditionsObject)
+    const hasConditions = conditions.values.length > 0
+    return {
+      text: `
+        SELECT *
+          FROM ${this.getFromClauseWithTableMetadata(tableMetadata)}
+          ${hasConditions ? `WHERE ${conditions.text}` : ''}
+      `,
+      values: conditions.values
+    }
+  }
+  getFromClauseWithTableMetadata(tableMetadata) {
+    return `${tableMetadata.schema}.${tableMetadata.name} as ${tableMetadata.name}`
+  }
+  getConditionsWithObject(conditionsObject) {
+    const conditionKeys = Object.keys(conditionsObject || {})
+    return {
+      text: conditionKeys.reduce((sql, conditionKey, index) => {
+        return `${sql}${sql.length !== 0 ? ' AND ' : ''}${conditionKey} = $${index+1}`
+      }, ''),
+      values: conditionKeys.map(key => conditionsObject[key])
+    }
+  }
+}
+
+module.exports = QueryBuilder
