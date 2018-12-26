0b1c9beb57d910a0c4fd3c11d71561b19d054235
Jérémy Touzy <jtouzy2@gmail.com>
2018-12-26 10:57:16 +0100

    feat: improve Dao instance building + add tableMetadata getter

--- src/Nordic.js
+++ src/Nordic.js
@@ -7,8 +7,9 @@ class Nordic {
     this.$databaseProxy = new DatabaseProxy({ host, port, database, user, password })
   }
   async getDao(daoClassOrTableName) {
+    let daoInstance
     if (typeof daoClassOrTableName === 'function') {
-      return new daoClassOrTableName(this.$databaseProxy)
+      daoInstance = new daoClassOrTableName()
     } else {
       let context = {}
       if (typeof daoClassOrTableName === 'string') {
@@ -19,14 +20,30 @@ class Nordic {
       } else {
         throw new Error(`Can't create Dao instance`)
       }
-      return new Dao(context, this.$databaseProxy)
+      daoInstance = new Dao(context)
     }
+    // Inject
+    return Object.assign(daoInstance, {
+      $databaseProxy: this.$databaseProxy,
+      $tableMetadata: await this.getTableMetadata(daoInstance.$context)
+    })
   }
   $getDaoContextFromString(table) {
     return { schema: 'public', table }
   }
   $getDaoContextFromObject(schema, table) {
     return { schema, table }
+  }
+  async getTableMetadata({ schema, table }) {
+    const databaseMetadata = await this.getDatabaseMetadata()
+    if (!Object.keys(databaseMetadata).find(s => s === schema)) {
+      throw new Error(`Missing schema '${schema}' in database metadata.`)
+    }
+    const tableMd = databaseMetadata[schema].find(t => t.name === table)
+    if (!tableMd) {
+      throw new Error(`Missing table '${table}' in '${schema}' schema metadata.`)
+    }
+    return tableMd
   }
   async getDatabaseMetadata() {
     if (!this.$databaseMetadata) {
@@ -37,6 +54,7 @@ class Nordic {
         return Object.assign(accumulator, {
           [table.table_schema]: (accumulator[table.table_schema] || []).concat([{
             name: table.table_name,
+            schema: table.table_schema,
             columns: tableColumns.filter(c => {
               return c.table_schema === table.table_schema && c.table_name === table.table_name
             }).map(c => {
