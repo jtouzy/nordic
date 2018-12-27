3e00ed29b830973c96512de861e401798e1a7c0d
Jérémy TOUZY <jtouzy2@gmail.com>
2018-12-27 13:41:00 +0100

    test: add EntityContextFactory unit tests

--- test/dao/EntityContextFactory.test.js
+++ test/dao/EntityContextFactory.test.js
@@ -28,6 +28,14 @@ describe('EntityContextFactory.from', () => {
   it('Should throw error if nothing is given', () => {
     expect(() => { EntityContextFactory.from() }).to.throw(Error)
   })
+  it('Should resolve context if a string entity is given', () => {
+    const context = EntityContextFactory.from('articles')
+    expect(context).to.be.eql({ schema: 'public', table: 'articles' })
+  })
+  it('Should resolve context if an object entity is given', () => {
+    const context = EntityContextFactory.from({ schema: 'secured', table: 'articles' })
+    expect(context).to.be.eql({ schema: 'secured', table: 'articles' })
+  })
   it('Should resolve context if a class with string entity is given', () => {
     const context = EntityContextFactory.from(EntityProviderMockWithValidStringValue)
     expect(context).to.be.eql({ schema: 'public', table: 'articles' })
