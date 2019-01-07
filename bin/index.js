a30c1a1581c58453d8f590d6cccd83f990038e56
Jérémy Touzy <jtouzy2@gmail.com>
2019-01-07 19:40:25 +0100

    feat(bin): add cl tool to generate md (2)

--- bin/index.js
+++ bin/index.js
@@ -2,18 +2,52 @@
 
 const fs = require('fs')
 const path = require('path')
+const readline = require('readline')
 const nordic = require('../src/nordic')
+
+const interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const askForOption = (option, nextIndex, options, object, callback) => {
+  interface.question(option.text, (answer) => {
+    const answers = Object.assign({}, object, {
+      [option.id]: answer
+    })
+    if (Object.keys(answers).length === options.length) {
+      callback(answers)
+    } else {
+      askForOption(options[nextIndex], nextIndex + 1, options, answers, callback)
+    }
+  })
+}
 
 const handleMetadataCommand = async () => {
   const args = process.argv
   const workingDirectory = process.cwd()
-  //nordic.initialize({
-  //})
-  const dbMetadata = await nordic.getDatabaseMetadata()
-  fs.writeFileSync(
-    path.resolve(workingDirectory, 'nordic-metadata.json'),
-    JSON.stringify(dbMetadata, null, 2)
-  )
+  return new Promise((resolveFn, rejectFn) => {
+    const options = [
+      { id: 'host',     text: 'Host     : ' },
+      { id: 'port',     text: 'Port     : ' },
+      { id: 'database', text: 'Database : ' },
+      { id: 'user',     text: 'User     : ' },
+      { id: 'password', text: 'Password : ' }
+    ]
+    askForOption(options[0], 1, options, {}, (answers) => {
+      nordic.initialize(answers)
+      nordic.getDatabaseMetadata().then((dbMetadata) => {
+        fs.writeFileSync(
+          path.resolve(workingDirectory, 'nordic-metadata.json'),
+          JSON.stringify(dbMetadata, null, 2)
+        )
+        nordic.shutdown().then(() => {
+          console.log('nordic: metadata generated successfully.')
+          resolveFn()
+        }).catch(rejectFn)
+      }).catch(rejectFn)
+    })
+  })
 }
 
 const handleCommand = async () => {
@@ -31,7 +65,10 @@ const handleCommand = async () => {
   }
 }
 
-handleCommand().catch((error) => {
+handleCommand().then(() => {
+  process.exit()
+}).catch((error) => {
   console.log(`nordic: [error] ${error}`)
+  nordic.shutdown().catch((ne) => {})
   process.exit(1)
 })
